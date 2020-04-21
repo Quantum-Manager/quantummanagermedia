98d15da5c6b9904bde2f4912187606d840651370
Dmitrii <tsymbal.mitia@gmail.com>
2020-04-21 23:18:24 +0300

    fix

--- tmpl/default.php
+++ tmpl/default.php
@@ -149,7 +149,7 @@ else
 
 <script type="text/javascript">
     window.QuantumwindowLang = {
-        'buttonInsert': '<?php echo Text::_('COM_QUANTUMMANAGER_WINDOW_INSERT'); ?>',
+        'buttonInsert': '<?php echo Text::_('COM_QUANTUMMANAGER_ACTION_PASTE'); ?>',
         'inputAlt': '<?php echo Text::_('PLG_QUANTUMMANAGERMEDIA_WINDOW_ALT'); ?>',
         'inputWidth': '<?php echo Text::_('PLG_QUANTUMMANAGERMEDIA_WINDOW_WIDTH'); ?>',
         'inputHeight': '<?php echo Text::_('PLG_QUANTUMMANAGERMEDIA_WINDOW_HEIGHT'); ?>',
