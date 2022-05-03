21cb939638614b6021d666329dc6a8481ae7ca30
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2022-05-03 11:39:01 +0300

    update script file

--- script.php
+++ script.php
@@ -24,21 +24,17 @@ class plgSystemQuantummanagermediaInstallerScript
 	/**
 	 * Called after any type of action
 	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   string            $route    Which action is happening (install|uninstall|discover_install|update)
 	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function postflight($route, $adapter) {
-
-		if (version_compare((new Version())->getShortVersion(), '4.0', '<'))
-		{
-			$db = Factory::getDbo();
-			$query = $db->getQuery( true );
-			$query->update( '#__extensions' )->set( 'enabled=1' )->where( 'type=' . $db->q( 'plugin' ) )->where( 'element=' . $db->q( 'quantummanagermedia' ) );
-			$db->setQuery( $query )->execute();
-		}
-
+	public function postflight($route, $adapter)
+	{
+		$db    = Factory::getDbo();
+		$query = $db->getQuery(true);
+		$query->update('#__extensions')->set('enabled=1')->where('type=' . $db->q('plugin'))->where('element=' . $db->q('quantummanagermedia'));
+		$db->setQuery($query)->execute();
 	}
 
 }
