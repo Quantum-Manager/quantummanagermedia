f7170e30b8135738fb3fee30d3648163e1e95022
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2022-11-24 07:01:49 +0300

    add check install component

--- quantummanagermedia.php
+++ quantummanagermedia.php
@@ -61,7 +61,7 @@ class plgSystemQuantummanagermedia extends CMSPlugin
 	{
 		parent::__construct($subject, $config);
 
-		if(file_exists(JPATH_SITE . '/administrator/components/com_quantummanager/helpers/quantummanager.php'))
+		if(file_exists(JPATH_SITE . '/administrator/components/com_quantummanager/quantummanager.php'))
 		{
 			$this->install_quantummanager = true;
 		}
